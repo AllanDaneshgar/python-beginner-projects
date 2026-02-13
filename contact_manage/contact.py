2c8b436a42d516b56d9d8a96b11ddb2b84eda447
Allan Daneshgar <daneshgarallan88@gmail.com>
2026-02-13 22:17:34 +0330

    renamed calculator/READM.md and modified contact.py

--- contact_manage/contact.py
+++ contact_manage/contact.py
@@ -1,10 +1,12 @@
 import json
 import os
+import re
 from datetime import datetime
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-class Contact():
-    def __init__(self, file_name=".contact.json"):
-        self.filename = file_name
+class ContactManager():
+    def __init__(self, file_name="contact.json"):
+        self.filename = os.path.join(BASE_DIR, file_name)
         self.contacts = []
         self.load_contacts()
 
@@ -30,7 +32,60 @@ class Contact():
     def get_new_id(self):
         if self.contacts:
             return max(c["id"] for c in self.contacts) + 1
-        return 1
+        else:
+            return 1
+
+    def add_contact(self):
+        print("\n➕ ADD NEW CONTACT")
+        print("-" * 20)
+
+        name = input("Name: ").strip()
+        for contact in self.contacts:
+            if contact["name"].lower() == name.lower():
+                print("❌ This Contact already exists!")
+                return
+        if not name:
+            print("❌ Name cannot be empty!\n Defult Value => None")
+            return
+
+
+        phone = input("phone: ").strip()
+        if not phone:
+            print("❌ Phone cannot be empty!\n Defult Value => None")
+            return
+        elif len(phone) > 11:
+            print("❌ The phone number cannot exceed 11 digits!\n Defult Value => 0")
+            return
+
+        email = input("email: ").strip()
+        if not email:
+            print("❌ email cannot be empty!\nDefault Value => None")
+            return
+        regex = r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
+        if not re.match(regex, email):
+            print("❌ invalid email!")
+            return
+
+        print("Groups: family, friends, work")
+        group = input("Group: ").strip().lower()
+        if group not in ["family", "friends", "work"]:
+            print("⚠️ Invalid group! Using 'friends'")
+            group = "friends"
+
+        current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+        new_contact = {
+            "id": self.get_new_id(),
+            "name": name,
+            "phone": phone,
+            "email": email,
+            "group": group,
+            "time": current_time
+        }
+
+        self.contacts.append(new_contact)
+        self.save_contacts()
+        print(f"✅ Contact added (ID: {new_contact['id']})")
 
 def show_menu():
     print("\n" + "-"*30)
@@ -38,17 +93,37 @@ def show_menu():
     print("2. 👥 View all contacts")
     print("3. 🔍 Search contacts")
     print("4. 🗑️ Delete contact")
-    print("5. 🚪 Exit")
+    print("5. 📊 Show statistics")
+    print("6. 🚪 Exit")
 
     try:
-        choice = int(input("Choose (1-7): "))
-        return choice if 1 <= choice <= 7 else None
+        choice = int(input("Choose (1-6): "))
+        return choice if 1 <= choice <= 6 else None
     except ValueError:
         return None
 
 
 def main():
-    print("")
+    print("=" * 50)
+    print("📞 WELCOME TO CONTACT MANAGER")
+    print("Contacts are saved in 'contacts.json'")
+
+    manager = ContactManager()
+
+    while True:
+        choice = show_menu()
+        if choice == 1:
+            manager.add_contact()
+
+        elif choice == 6:
+            print("\n👋 Goodbye!")
+            print("Thank you for using Contact Manager!")
+            break
+        else:
+            print("⚠️ Please choose 1-7")
+
+
+
 
 if __name__ == "__main__":
     main()
